e06909ef098fc9d0c0751354d9a6f5da8a43122b
Ilya324235 <122685983+Ilya324235@users.noreply.github.com>
2023-01-14 17:33:31 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+function handle() {
+    let inputs = document.querySelectorAll("input");
+    let input_values = [];
+    for (let i = 0; i < inputs.length; i++) {
+        input_values.push(inputs[i].value);
+    }
+ alert(inputs_values)
+    
+}
